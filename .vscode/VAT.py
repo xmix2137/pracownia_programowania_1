d03e2ce3cb033227a6175a249357e32c422565a6
Krzysztof Sikora <226458@student.uek.krakow.pl>
2022-10-15 16:59:58 +0200

    ostatni na dziś

---
+++ .vscode/VAT.py
@@ -0,0 +1,7 @@
+#Obliczanie podatku VAT
+
+
+a = float(input("Podaj kwotę od której chcesz obliczyć VAT: "))
+b = round(a*0.23, 2)
+print("Kwota to : ", a, "zŁ")
+print("Naliczony podatek VAT od tej kwoty to: ", b, "zł")
